3123be72e4cad4d3d5bee78010e1f28645dbfd4e
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2023-07-24 20:25:37 +0400

    Initial Commit

---
+++ ContactList_swiftUI/ContactList_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_swiftUIApp.swift
+//  ContactList_swiftUI
+//
+//  Created by Aleksandr Mamlygo on 24.07.23.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
